d2b2435a1a62ea7cde7e455bcf012f9b76fe6b77
andres hencker <pipe.henker117@gmail.com>
2021-09-16 11:19:05 -0500

    configración capa persistencia

---
+++ NutriTic.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,7 @@
+namespace NutriTic.App.Persistencia.AppRepositorios
+{
+    public class AppContext
+    {
+        
+    }
+}
--- NutriTic.App.Persistencia/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NutriTic.App.Persistencia
-{
-    public class Class1
-    {
-    }
-}
