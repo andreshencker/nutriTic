ef64948d47cea34f784df32ca72e5a45e493947a
andres hencker <pipe.henker117@gmail.com>
2021-09-18 13:59:47 -0500

    cambios

---
+++ NutriTic.App.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NutriTic.App.Dominio.Entidades;
+
+namespace NutriTic.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
