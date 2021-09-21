90b4391b082846cf03c1dcaa42c210c7277ddd77
Silva <marcoyezidsilva@gmail.com>
2021-09-21 15:10:54 -0500

    crud paciente

--- NutriTic.App.Dominio/Entidades/Paciente.cs
+++ NutriTic.App.Dominio/Entidades/Paciente.cs
@@ -3,6 +3,7 @@ namespace NutriTic.App.Dominio.Entidades
 {
     public class Paciente
     {//hola
+    
         public DateTime FechaNacimiento{get;set;}
         public int Estatura{get;set;}
         public string Latitud{get;set;}
